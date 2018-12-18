f1052e2471a0a17f50ae9200be937eefe4e412a0
r2newell <42938289+r2newell@users.noreply.github.com>
2018-12-18 13:25:54 -0500

    Update utility.h

--- utility.h
+++ utility.h
@@ -1,11 +1,10 @@
 #ifndef UTILITY_H_ 
 #define UTILITY_H_ 
-void display(const int *, int); 
-void generate(int *, int); 
+void display(const int *, int); // this function displays a sequence as its name suggest
+void generate(int *, int); //generates a pseudorandom sequence using the congruential method
 void selectionSort(int *, int); 
 void bubbleSort(int *, int); 
 void insertionSort(int *, int); //non decreasing order 
 void mergeSort(int *, int, int); 
 void merge(int *, int, int, int); 
-int binarySearch(const int *, int, int); 
 #endif
